aade35fa9ffde2097655cb621dee69b63a208285
denniswong10 <48814651+denniswong10@users.noreply.github.com>
2023-09-08 17:10:22 +0800

    Added of Storage Package

---
+++ Assets/Scripts/Storage Scripts/StorageItemScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StorageItemScript : MonoBehaviour
+{
+    private StorageComponent component;
+    private ItemTag item;
+
+    #region SETUP
+    public void SetItemComponent(StorageComponent component, ItemTag item)
+    {
+        this.component = component;
+        this.item = item;
+    }
+    #endregion
+
+    #region MAIN
+    public void SelectItem()
+    {
+        component.GetItemSelection(item);
+    }
+    #endregion
+}
