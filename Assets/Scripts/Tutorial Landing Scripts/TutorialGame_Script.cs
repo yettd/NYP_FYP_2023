895c5fb8c5c22cfe2e9e6a6d04c115177e5555d6
denniswong10 <48814651+denniswong10@users.noreply.github.com>
2023-09-08 17:11:56 +0800

    Added Tutorial Game Interface

---
+++ Assets/Scripts/Tutorial Landing Scripts/TutorialGame_Script.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialGame_Script : MonoBehaviour
+{
+    public void Back()
+    {
+        SceneManager.LoadScene("Tutorial_DashBoard");
+    }
+}
