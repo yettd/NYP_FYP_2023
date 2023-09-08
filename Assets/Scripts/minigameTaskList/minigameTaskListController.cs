0b3ab9baa06926cef010c1c5b305fd7a3c73f29f
yettd <junkai.poh@gmail.com>
2023-09-08 11:58:28 +0800

    added saving, minigametasklist basic, updated collection
    added a saving that can be use thorughout the programe
    
    a minigametask list to check if minigame is done
    
    make the collection tool page more readable

--- Assets/Scripts/minigameTaskList/minigameTaskListController.cs
+++ Assets/Scripts/minigameTaskList/minigameTaskListController.cs
@@ -4,88 +4,101 @@ using UnityEngine;
 
 public class minigameTaskListController : MonoBehaviour
 {
+    private enum Steps
+    {
+        //scaling
+        LOCATINGS,
+        CHOOSINGS,
+        SCRAPINGS,
+        END_TASKS,
+        //extraion
+
+        LOCATINGE,
+        CHOOSINGE,
+        SCRAPINGE,
+        END_TASKE,
+        //filling
+
+        LOCATINGF,
+        CHOOSINGF,
+        SCRAPINGF,
+        END_TASKF
+
+
+    }
     public static minigameTaskListController Instance;
 
-    private scaling currentScalingTask;
-    private filling currentFillingTask;
-    private extraction currentExtraionTask;
+    private Steps currentStep;
+    private Steps NextSteps;
 
     public Procedure procedure;
     private void Awake()
     {
-        currentScalingTask = scaling.LOCATING;
-        currentFillingTask = filling.LOCATING;
-        currentExtraionTask = extraction.LOCATING;
+
         if(Instance)
         {
             Instance = this;
-        }    
+        }
+        startminigame();
     }
 
-    public scaling GetCurrentScalingStep()
-    {
-        return currentScalingTask;
-    }
-    public filling GetCurrentFillingTaskStep()
-    {
-        return currentFillingTask;
-    }
-    public extraction GetCurrentScakingStep()
-    {
-        return currentExtraionTask;
-    }
+
+
+
 
     public bool gonext()
     {
-        
-        switch(procedure)
+        if (DoneWithMiniGame())
         {
-            case Procedure.Scaling:
-                //see if next is complete
-                currentScalingTask++;
-                Debug.Log(currentScalingTask);
-                //
-                break;
+            Debug.Log("Done ");
+            return true;
+        }
+        currentStep=NextSteps;
+        NextSteps++;
+       // Debug.Log($"{currentStep} : {NextSteps}");
+        return false;
+    }
 
-            case Procedure.Extration:
-
-                currentExtraionTask++;
-                Debug.Log(currentScalingTask);
-                break;
-            case Procedure.Filling:
-                break;
-                
+    public bool DoneWithMiniGame()
+    {
+        if (NextSteps.ToString().Contains("END"))
+        {
+            return true;
         }
 
         return false;
     }
 
+    public void startminigame()
+    {
+        switch(procedure)
+        {
+            case Procedure.Scaling:
+                currentStep = Steps.LOCATINGS;
+                NextSteps = currentStep + 1;
+              //  Debug.Log($"{currentStep} : {NextSteps}");
+                    break;
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            gonext();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            gonext();
+        }
+    }
 
 
 
 }
-public enum scaling
-{
-    LOCATING=1,
-    CHOOSING=2,
-    SCRAPING=3,
-    END_TASK=4
-}
-public enum filling
-{
-    LOCATING,
-    CHOOSING,
-    SCRAPING,
-    END_TASK
-}
+//put task here
 
-public enum extraction
-{
-    LOCATING=1,
-    CHOOSING=2,
-    SCRAPING=3,
-    END_TASK
-}
+
 
 public enum Procedure
 {
