7631703935b7b32b89b9128e08af5f67a99f6e38
denniswong10 <48814651+denniswong10@users.noreply.github.com>
2023-09-12 18:05:38 +0800

    Modify of Tutorial Game Script

--- Assets/Scripts/Tutorial Landing Scripts/TutorialGame_Script.cs
+++ Assets/Scripts/Tutorial Landing Scripts/TutorialGame_Script.cs
@@ -5,8 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class TutorialGame_Script : MonoBehaviour
 {
+    private DataManageScript data;
+    private const string fileDirectory = "savefile_tutorial_";
+
+    #region SETUP
+    private void UpdateInstructionStatus(bool cleared)
+    {
+        InstructionManual temp;
+        data = new DataManageScript("Assets/Resources/TutorialLevel/meta/", fileDirectory + TutorialNagivatorScript.thisScript.get_manual.name + ".txt");
+
+        if (data.FindFilePath()) // file existing will be overwritten to a new one
+            temp = data.LoadInfoThroughJson<InstructionManual>();
+
+        else // file doesn't exist will be created and written to a new one
+            temp = TutorialNagivatorScript.thisScript.get_manual;
+
+        temp.cleared.completed = cleared;
+        data.SaveInfoAsNewJson(temp);
+    }
+    #endregion
+
+    #region MAIN
     public void Back()
     {
-        SceneManager.LoadScene("Tutorial_LandingScene");
+        SetClearedCondition(true);
+        SceneManager.LoadScene(TutorialNagivatorScript.thisScript.GetTitleScene());
     }
+
+    public void SetClearedCondition(bool condition)
+    {
+        UpdateInstructionStatus(condition);
+        Debug.Log("Game Condition have been set to " + (condition ? "COMPLETE" : "NOT COMPLETE"));
+    }
+    #endregion
 }
