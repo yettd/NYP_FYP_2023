861ec52300208f72fb0b612b7b24ef11f98fe227
yettd <junkai.poh@gmail.com>
2023-09-13 18:15:35 +0800

    clean work

--- Assets/Scripts/Season2Scripts/TeethDirtClean.cs
+++ Assets/Scripts/Season2Scripts/TeethDirtClean.cs
@@ -13,34 +13,55 @@ using System.Collections;
 
 public class TeethDirtClean : MonoBehaviour
 {
-    public Camera cam;
+   [SerializeField] private Camera _camera;
 
-    void Start()
+    [SerializeField] private Texture2D _dirtMaskBase;
+    [SerializeField] private Texture2D _brush;
+
+    [SerializeField] private Material _material;
+
+    private Texture2D _templateDirtMask;
+
+    private void Start()
     {
-        cam = GetComponent<Camera>();
+        CreateTexture();
     }
 
-    void Update()
+    private void Update()
     {
-        if (!Input.GetMouseButton(0))
-            return;
+        if (Input.GetMouseButton(0))
+        {
+            if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+            {
+                Vector2 textureCoord = hit.textureCoord;
 
-        RaycastHit hit;
-        if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit))
-            return;
+                int pixelX = (int)(textureCoord.x * _templateDirtMask.width);
+                int pixelY = (int)(textureCoord.y * _templateDirtMask.height);
+                Debug.Log(pixelX);
+                for (int x = 0; x < _brush.width; x++)
+                {
+                    for (int y = 0; y < _brush.height; y++)
+                    {
+                        Color pixelDirt = _brush.GetPixel(x, y);
+                        Color pixelDirtMask = _templateDirtMask.GetPixel(pixelX + x, pixelY + y);
 
-        Renderer rend = hit.transform.GetComponent<Renderer>();
-        MeshCollider meshCollider = hit.collider as MeshCollider;
+                        _templateDirtMask.SetPixel(pixelX + x,
+                            pixelY + y,
+                            new Color(0, pixelDirtMask.g * pixelDirt.g, 0));
+                    }
+                }
 
-        if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null)
-            return;
+                _templateDirtMask.Apply();
+            }
+        }
+    }
 
-        Texture2D tex = rend.material.mainTexture as Texture2D;
-        Vector2 pixelUV = hit.textureCoord;
-        pixelUV.x *= tex.width;
-        pixelUV.y *= tex.height;
+    private void CreateTexture()
+    {
+        _templateDirtMask = new Texture2D(_dirtMaskBase.width, _dirtMaskBase.height);
+        _templateDirtMask.SetPixels(_dirtMaskBase.GetPixels());
+        _templateDirtMask.Apply();
 
-        tex.SetPixel((int)pixelUV.x, (int)pixelUV.y, Color.black);
-        tex.Apply();
+        _material.SetTexture("_DirtMask", _templateDirtMask);
     }
 }
