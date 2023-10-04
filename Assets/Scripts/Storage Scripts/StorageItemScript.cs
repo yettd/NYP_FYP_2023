844dabf4d2366c78943cd93f2ef7a24a151336fe
denniswong10 <48814651+denniswong10@users.noreply.github.com>
2023-10-04 18:35:40 +0800

    Modify for Extraction Script

--- Assets/Scripts/Storage Scripts/StorageItemScript.cs
+++ Assets/Scripts/Storage Scripts/StorageItemScript.cs
@@ -6,11 +6,16 @@ public class StorageItemScript : MonoBehaviour
 {
     private StorageComponent component;
     private ItemTag item;
+    private DW_ToolsNagivator toolsNagivator;
+
     private GameObject itemModel;
     public moveTools MTS;
+
     #region SETUP
     public void SetItemComponent(StorageComponent component, ItemTag item)
     {
+        toolsNagivator = new DW_ToolsNagivator();
+
         this.component = component;
         this.item = item;
     }
@@ -19,11 +24,16 @@ public class StorageItemScript : MonoBehaviour
     #region MAIN
     public void SelectItem()
     {
+        // Select item for uses
         component.GetItemSelection(item);
+
+        // Target selected area on item currently used
+        if (TutorialNagivatorScript.Instance().get_manual.toolAccessId == 1) toolsNagivator.GetToolsOnUse(item.itemName);
     }
+
     public void SelectTool()
     {
-        minigameTaskListController.Instance.ToolsSelected(item.itemName, item.model);
+       if (TutorialNagivatorScript.Instance().get_manual.toolAccessId == 0) minigameTaskListController.Instance.ToolsSelected(item.itemName, item.model);
     }
     #endregion
 }
