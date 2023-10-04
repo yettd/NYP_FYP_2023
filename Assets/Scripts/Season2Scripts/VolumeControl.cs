df42e25234ccab7f61fed1e96ea41e7eed3dc24b
yettd <junkai.poh@gmail.com>
2023-10-04 11:46:37 +0800

    stuffings for turkey

--- Assets/Scripts/Season2Scripts/VolumeControl.cs
+++ Assets/Scripts/Season2Scripts/VolumeControl.cs
@@ -5,13 +5,33 @@ public class VolumeControl : MonoBehaviour
 {
     [SerializeField] private Slider volumeSlider;
 
+
+    vol v;
+
+
     private void Start()
     {
         volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1f);
         volumeSlider.onValueChanged.AddListener(AdjustVolume);
+        if(Saving.save.LoadSoundFromJson()!=null)
+        {
+
+        string a = Saving.save.LoadSoundFromJson();
+            v = JsonUtility.FromJson<vol>(a);
+        }
+
     }
     private void AdjustVolume(float volume)
     {
         AudioManager.Instance.SetVolume(volume);
+        v.volume = volume;
+
+        Saving.save.saveSoundToJson(v);
+
+    }
+
+    class vol
+    {
+        public float volume;
     }
 }
